702842423a566347dffd28c122faaebc0d23cb70
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2021-10-25 20:31:17 +0530

    Javaprogram

--- src/Tictactoe/Tictactoegame.java
+++ src/Tictactoe/Tictactoegame.java
@@ -1,10 +1,15 @@
 package Tictactoe;
 
+import java.util.Scanner;
+
 public class Tictactoegame {
+	char player_letter = ' ';
+	char computer_letter = ' ';
 public static void main(String[] args) {
 	System.out.println("Welcome to Tic Tac Toe game");
 	Tictactoegame rv1 = new Tictactoegame();
 	rv1.tictacboard();
+	rv1.user_choice();
 }
 void tictacboard()
 {
@@ -30,4 +35,22 @@ private static void printBoard(char[][] board) {
 			System.out.print("|");
 			System.out.println((board[2][2]));
 			}
+void user_choice() {
+	Scanner rv1 = new Scanner(System.in);
+	System.out.println("choose a letter x or o");
+	char user_input = rv1.next().charAt(0);
+	if (user_input == 'x') {
+		System.out.println("User player takes x and computer player takes o");
+		 player_letter = 'x';
+		 computer_letter = 'o';
+	}
+	else if (user_input == 'o') {
+		System.out.println("User player takes o and computer player takes x");
+		player_letter = 'o';
+		 computer_letter = 'x';
+	}
+	else {
+		System.out.println("Invalid character");
+	}
+}
 	}
